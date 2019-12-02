4fbf02c4faf43917c62ca057c4511523ae948cb2
Thomas Modeneis <thomas.modeneis@gmail.com>
2019-12-03 00:29:32 +0100

    Publishing to gh-pages (publish.sh)

---
+++ js/cade5cfd88f951602728.bundle.js
@@ -0,0 +1,15 @@
+(window["__webpackJsonp"] = window["__webpackJsonp"] || []).push([[0],{
+
+/***/ "./translation/en-US.json":
+/*!********************************!*\
+  !*** ./translation/en-US.json ***!
+  \********************************/
+/*! exports provided: title, description, general.error.notFound, general.loading, general.error, general.loadingText, toolbar.alethio.label, toolbar.localization.label, cookieBanner.label, cookieBanner.text, cookieBanner.accept, cookieBanner.moreInfo, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"title\":\"Smilo Lite Blockchain Explorer\",\"description\":\"Client only, super light weight and entirely open source Smilo blockchain explorer.\",\"general.error.notFound\":\"404 - Not Found\",\"general.loading\":\"Loading\",\"general.error\":\"An error has occurred\",\"general.loadingText\":\"Data is being loaded.<br />Please be patient, it won't be long now ;)\",\"toolbar.alethio.label\":\"Smilo Explorer website\",\"toolbar.localization.label\":\"Select Language\",\"cookieBanner.label\":\"Cookie Policy\",\"cookieBanner.text\":\"By using this site, you agree to our use of cookies, which we use to analyse our traffic in accordance with our %sPrivacy Policy%s . We also share information about your use of our site with our analytics partners\",\"cookieBanner.accept\":\"Accept\",\"cookieBanner.moreInfo\":\"More Info\"}");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=cade5cfd88f951602728.bundle.js.map
