327d4eaa1765ceb4a50bf0e058defefbdb000d6f
Thomas Modeneis <thomas.modeneis@gmail.com>
2019-12-03 00:40:45 +0100

    rebranding to smilo

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
-#!/bin/sh
-
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
+##!/bin/sh
+#
+#if [ "`git status -s`" ]
+#then
+#    echo "The working directory is dirty. Please commit any pending changes."
+#    exit 1;
+#fi
+#
+#echo "Deleting old publication"
+#rm -rf public
+#mkdir public
+#git worktree prune
+#rm -rf .git/worktrees/public/
+#
+#echo "Checking out gh-pages branch into public"
+#git worktree add -B gh-pages public origin/gh-pages
+#
+#echo "Removing existing files"
+#rm -rf public/*
+#
 mkdir public
 npm run build && cp -r dist/* public
+cp CNAME public/CNAME
 
 echo "Updating gh-pages branch"
 cd public && git pull && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
